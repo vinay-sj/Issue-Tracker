56e03828660d38a8bfd40551c891e31fd68172b6
vinaysj <srampickaljoseph.v@husky.neu.edu>
2020-05-17 14:58:24 -0400

    Implemented Support for Older Browsers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+const continents = ['Africa','America','Asia','Australia','Europe'];
+const helloContinents = Array.from(continents,c=>`Hello ${c}!`);
+const message = helloContinents.join(' ');
 const element = (
     <div title="Outer div">
-    <h1>Hello World!</h1></div>
+    <h1>{message}</h1></div>
 );
 ReactDOM.render(element, document.getElementById('content'));
