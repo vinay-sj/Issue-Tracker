cefa412f28816ad33746ad432fd46b85e0872036
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-06-23 08:42:26 -0400

    separated api_handler.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,8 @@
-const fs = require('fs');
-require('dotenv').config();
+// const fs = require('fs');
+// require('dotenv').config();
 const express = require('express');
-const { ApolloServer, UserInputError } = require('apollo-server-express');
-const GraphQLDate = require('./graphql_date.js');
+// const { ApolloServer, UserInputError } = require('apollo-server-express');
 const { MongoClient } = require('mongodb');
-const about= require('./about.js');
 const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 const port = process.env.API_SERVER_PORT || 3000;
 let db;
@@ -46,38 +44,13 @@ async function issueAdd(_, { issue }) {
   return savedIssue;
 }
 
-const resolvers = {
-  Query: {
-    about: about.getMessage,
-    issueList,
-  },
-  Mutation: {
-    setAboutMessage: about.setMessage,
-    issueAdd,
-  },
-  GraphQLDate,
-};
-
 async function connectToDb() {
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
   db = client.db();
 }
-
-const server = new ApolloServer({
-  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
-  resolvers,
-  formatError: (error) => {
-    console.log(error);
-    return error;
-  },
-});
-
 const app = express();
-const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
-console.log('CORS setting:', enableCors);
-server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
 
 (async function start() {
   try {
