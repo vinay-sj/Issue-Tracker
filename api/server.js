58a3e2e4087790ca82117d07137284b1f35369a9
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-06-23 09:02:17 -0400

    separated db.js and issue.js from server.js

--- api/server.js
+++ api/server.js
@@ -1,56 +1,11 @@
-// const fs = require('fs');
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
-// const { ApolloServer, UserInputError } = require('apollo-server-express');
-const { MongoClient } = require('mongodb');
-const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
-const port = process.env.API_SERVER_PORT || 3000;
-let db;
+const { connectToDb } = require('./db.js');
+const { installHandler } = require('./api_handler');
 
-async function issueList() {
-  const issues = await db.collection('issues').find({}).toArray();
-  return issues;
-}
-
-function issueValidate(issue) {
-  const errors = [];
-  if (issue.title.length < 3) {
-    errors.push('Field "title" must be at least 3 characters long.');
-  }
-  if (issue.status === 'Assigned' && !issue.owner) {
-    errors.push('Field "owner" is required when status is "Assigned"');
-  }
-  if (errors.length > 0) {
-    throw new UserInputError('Invalid input(s)', { errors });
-  }
-}
-
-async function getNextSequence(name) {
-  const result = await db.collection('counters').findOneAndUpdate(
-    { _id: name },
-    { $inc: { current: 1 } },
-    { returnOriginal: false },
-  );
-  return result.value.current;
-}
-
-async function issueAdd(_, { issue }) {
-  issueValidate(issue);
-  const newIssue = Object.assign({}, issue);
-  newIssue.created = new Date();
-  newIssue.id = await getNextSequence('issues');
-  const result = await db.collection('issues').insertOne(newIssue);
-  const savedIssue = await db.collection('issues').findOne({ _id: result.insertedId });
-  return savedIssue;
-}
-
-async function connectToDb() {
-  const client = new MongoClient(url, { useNewUrlParser: true });
-  await client.connect();
-  console.log('Connected to MongoDB at', url);
-  db = client.db();
-}
 const app = express();
+installHandler(app);
+const port = process.env.API_SERVER_PORT || 3000;
 
 (async function start() {
   try {
