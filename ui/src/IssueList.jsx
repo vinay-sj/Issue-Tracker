a316dc14c79b0417edcb8cbea13ad3cf675fb807
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-07-03 03:14:55 -0400

    used bootstrap Label component

--- ui/src/IssueList.jsx
+++ ui/src/IssueList.jsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import URLSearchParams from 'url-search-params';
 import { Route } from 'react-router-dom';
+import { Label } from 'react-bootstrap';
 import IssueFilter from './IssueFilter.jsx';
 import IssueTable from './IssueTable.jsx';
 import IssueAdd from './IssueAdd.jsx';
@@ -123,7 +124,7 @@ export default class IssueList extends React.Component {
     const { match } = this.props;
     return (
       <React.Fragment>
-        <h1>Issue Tracker</h1>
+        <h1><Label>Issue Tracker</Label></h1>
         <IssueFilter />
         <hr />
         <IssueTable
