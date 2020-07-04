ce801c76ed81ae48f449c839f43254a08a482bf3
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-07-03 23:40:34 -0400

    implemented Toast component in issueEdit

--- ui/src/IssueEdit.jsx
+++ ui/src/IssueEdit.jsx
@@ -9,6 +9,7 @@ import graphQLFetch from './graphQLFetch.js';
 import NumInput from './NumInput.jsx';
 import DateInput from './DateInput.jsx';
 import TextInput from './TextInput.jsx';
+import Toast from './Toast.jsx';
 
 export default class IssueEdit extends React.Component {
   constructor() {
@@ -17,10 +18,16 @@ export default class IssueEdit extends React.Component {
       issue: {},
       invalidFields: {},
       showingValidation: false,
+      toastVisible: false,
+      toastMessage: ' ',
+      toastType: 'success',
     };
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.onValidityChange = this.onValidityChange.bind(this);
+    this.showSuccess = this.showSuccess.bind(this);
+    this.showError = this.showError.bind(this);
+    this.dismissToast = this.dismissToast.bind(this);
   }
 
   componentDidMount() {
@@ -70,10 +77,10 @@ export default class IssueEdit extends React.Component {
       }
       }`;
     const { id, created, ...changes } = issue;
-    const data = await graphQLFetch(query, { changes, id });
+    const data = await graphQLFetch(query, { changes, id }, this.showError);
     if (data) {
       this.setState({ issue: data.issueUpdate });
-      alert('Updated issue successfully'); // eslint-disable-line no-alert
+      this.showSuccess('Updated issue successfully');
     }
   }
 
@@ -86,7 +93,7 @@ export default class IssueEdit extends React.Component {
     }`;
 
     const { match: { params: { id } } } = this.props;
-    const data = await graphQLFetch(query, { id: parseInt(id, 10) });
+    const data = await graphQLFetch(query, { id: parseInt(id, 10) }, this.showError);
     this.setState({ issue: data ? data.issue : {}, invalidFields: {} });
   }
 
@@ -98,9 +105,26 @@ export default class IssueEdit extends React.Component {
     this.setState({ showingValidation: false });
   }
 
+  showSuccess(message) {
+    this.setState({
+      toastVisible: true, toastMessage: message, toastType: 'success',
+    });
+  }
+
+  showError(message) {
+    this.setState({
+      toastVisible: true, toastMessage: message, toastType: 'danger',
+    });
+  }
+
+  dismissToast() {
+    this.setState({ toastVisible: false });
+  }
+
   render() {
     const { issue: { id } } = this.state;
     const { match: { params: { id: propsId } } } = this.props;
+    const { toastVisible, toastMessage, toastType } = this.state;
     if (id == null) {
       if (propsId != null) {
         return <h3>{`Issue with ID ${propsId} not found.`}</h3>;
@@ -241,6 +265,13 @@ export default class IssueEdit extends React.Component {
           {' | '}
           <Link to={`/edit/${id + 1}`}>Next</Link>
         </Panel.Footer>
+        <Toast
+          showing={toastVisible}
+          onDismiss={this.dismissToast}
+          bsStyle={toastType}
+        >
+          {toastMessage}
+        </Toast>
       </Panel>
     );
   }
